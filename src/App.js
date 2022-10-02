6d47e0cfd6fa76bfdb1d548fc023af5cd18663a8
ayanbajo <bolatitoayanbajo@gmail.com>
2022-10-02 11:57:29 -0400

    Writing Thunk

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import { useSelector, useDispatch } from 'react-redux'
+import {clearData, incrementId, decrementId, inputId, fetchData} from './features/dataSlice'
 
 function App() {
-  
+  // your logic goes here!
+  const dispatch = useDispatch()
+  const data = useSelector ((state) => state.data)
+
+  const renderImg = () => {
+    if (data.apiData) { //if() == true
+      console.log(data.apiData.title)
+      return <img style={{'width': '80vw'}} src={data.apiData.pimaryImage} alt={data.apiData.title} />
+    } else{
+      return <p>No Image Found</p>
+    }
+  }
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
-      <Nav />
-      <ContentWrapper />
-      <Footer />
+    <div className="App">
+      <div>
+        <button onClick={() => {dispatch(fetchData())}}>Trigger Thunk</button>
+        <button onClick={() => {dispatch(clearData())}}>Clear</button>
+        <button onClick={() => {dispatch(incrementId())}}>Next</button>
+        <button onClick={() => {dispatch(decrementId())}}>Back</button>
+      </div>
+      <input value={data.objectId} onChange={(e) => { dispatch(inputId(Number(e.target.value))) }} />
+      <div>
+        {data.objectId}
+        {renderImg}
+      </div>
     </div>
   );
 }
